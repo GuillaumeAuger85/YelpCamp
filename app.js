de8f51604a1cfd5eff4620676e20d57daaf602e5
augerg <auger.guillaume@gmail.com>
2022-06-28 18:00:03 -0400

    correction cookie.secure

--- app.js
+++ app.js
@@ -71,11 +71,12 @@ const sessionConfig = {
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        // secure: true,
+        secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 };
+app.set('trust proxy', 1);
 app.use(session(sessionConfig));
 app.use(flash());
 
