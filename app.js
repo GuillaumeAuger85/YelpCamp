095fd753d2884304ec77119564ed7605636f7056
augerg <auger.guillaume@gmail.com>
2022-06-28 17:18:55 -0400

    First deploying

--- app.js
+++ app.js
@@ -51,6 +51,8 @@ app.use(mongoSanitize());
 
 const secret = process.env.SECRET || 'thisshouldbeabettersecret!'
 
+// const secret ='thisshouldbeabettersecret!'
+
 const store = MongoDBStore.create({
     mongoUrl: dbURL,
     secret,
